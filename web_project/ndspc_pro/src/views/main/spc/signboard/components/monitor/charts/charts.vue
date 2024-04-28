2958449ccc65dcc32c32af33da91ea8d2ad56fc5
unknown <2281224649@qq.com>
2024-04-28 10:51:37 +0800

    运行监控看板图编写

---
+++ web_project/ndspc_pro/src/views/main/spc/signboard/components/monitor/charts/charts.vue
@@ -0,0 +1,204 @@
+<template>
+  <div ref="chart" :style="{'width': boxWidth+'px', 'height': '100px'}"></div>
+</template>
+
+<script setup>
+import * as echarts from 'echarts/core'
+import { DatasetComponent, TooltipComponent, VisualMapComponent } from 'echarts/components'
+import { CustomChart } from 'echarts/charts'
+import { CanvasRenderer } from 'echarts/renderers'
+echarts.use([DatasetComponent, TooltipComponent, VisualMapComponent, CustomChart, CanvasRenderer])
+import * as d3 from 'd3'
+import { ref, onMounted,nextTick } from 'vue'
+
+const props = defineProps({
+  boxWidth: {
+    type: Number,
+    default: 300
+  },
+})
+const chart = ref(null)
+
+const colorList = [
+  '#10904a',
+  '#2fc06f',
+  '#da998c',
+  '#f7e89b',
+  '#fff000',
+]
+
+let option = {}
+let seriesData = [
+  {
+    depth: 1,
+    id: 'city',
+    index: 0,
+    value: 2
+  },
+  {
+    depth: 1,
+    id: 'city.孝感',
+    index: 6,
+    value: 6
+  },
+  {
+    depth: 1,
+    id: 'city.武汉',
+    index: 1,
+    value: 36
+  },
+  {
+    depth: 1,
+    id: 'city.荆州',
+    index: 2,
+    value: 26
+  },
+  {
+    depth: 1,
+    id: 'city.咸宁',
+    index: 3,
+    value: 16
+  }
+]
+
+let displayRoot = stratify1()
+
+function stratify1() {
+  return d3
+    .stratify()
+    .parentId(function (d) {
+      return d.id.substring(0, d.id.lastIndexOf('.'))
+    })(seriesData)
+    .sum(function (d) {
+      return d.value || 0
+    })
+    .sort(function (a, b) {
+      return b.value - a.value
+    })
+}
+
+function overallLayout(params, api) {
+  let context = params.context
+  d3
+    .pack()
+    .size([api.getWidth() - 2, api.getHeight() - 2])
+    .padding(0)(displayRoot)
+  context.nodes = {}
+
+  displayRoot.descendants().forEach(function (node) {
+    context.nodes[node.id] = node
+  })
+}
+
+function renderItem(params, api) {
+  let context = params.context
+  let idx = params.dataIndex
+
+  // Only do that layout once in each time `setOption` called.
+  // 每次调用“setOption”时，只能进行一次布局。
+  if (!context.layout) {
+    context.layout = true
+    overallLayout(params, api)
+  }
+
+  let nodePath = api.value('id')
+  const nodeValue = api.value('value')
+  console.log('nodePath',nodeValue)
+  let nodeName = nodeValue
+  let node = context.nodes[nodePath]
+  if (node.id === 'city') {
+    node.r = 0
+  }
+  if (!node) {
+    // Reder nothing.
+    return
+  }
+
+  let colorTop = colorList[idx % colorList.length]
+
+  // 创建对象
+  let linearGradient = new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+    { offset: 0, color: colorTop },
+    { offset: 1, color: colorTop }
+  ])
+
+  let z2 = api.value('depth') * 2
+  return {
+    type: 'circle',
+    shape: {
+      cx: node.x,
+      cy: node.y,
+      r: node.r
+    },
+    transition: ['shape'],
+    z2: z2,
+    textContent: {
+      type: 'text',
+      style: {
+        text: nodeName,
+        fill: '#fff',
+        fontFamily: 'Arial',
+        width: node.r * 1.3,
+        overflow: 'truncate',
+        fontSize: Math.max(node.r / 2, 12)
+      },
+      emphasis: {
+        style: {
+          overflow: null,
+          fontSize: Math.max(node.r / 2, 12)
+        }
+      }
+    },
+    textConfig: {
+      position: 'inside'
+    },
+    style: {
+      fill: linearGradient
+    },
+    emphasis: {
+      style: {
+        fontFamily: 'Arial',
+        fontSize: 12,
+        shadowBlur: 20,
+        shadowOffsetX: 3,
+        shadowOffsetY: 5,
+        shadowColor: 'rgba(0,0,0,0.3)'
+      }
+    }
+  }
+}
+
+option = {
+  dataset: {
+    source: seriesData
+  },
+  tooltip: {},
+  hoverLayerThreshold: Infinity,
+  series: [
+    {
+      type: 'custom',
+      colorBy: 'data',
+      renderItem: renderItem,
+      progressive: 0,
+      coordinateSystem: 'none',
+      encode: {
+        tooltip: 'value',
+        itemName: 'id'
+      }
+    }
+  ]
+}
+
+const initEcharts = () => {
+  const myChart = echarts.init(chart.value)
+  myChart.setOption(option)
+}
+
+onMounted(() => {
+  nextTick(() => {
+    if(chart.value) {
+      initEcharts()
+    }
+  })
+})
+</script>
--- web_project/ndspc_pro/src/views/main/spc/signboard/components/monitor/charts/index.vue
+++ web_project/ndspc_pro/src/views/main/spc/signboard/components/monitor/charts/index.vue
@@ -1,10 +1,60 @@
 <template>
   <div class="card-list">
-    
+    <div class="card">
+      <div class="flex">
+        <div class="card-title defalt-width" ref="boxRef">工厂</div>
+        <div class="card-title defalt-width">车间</div>
+        <div class="card-title auto-width" ref="autoRef">产线</div>
+      </div>
+      <div class="charBox flex" v-for="(item, index) in list" :key="index">
+        <div class="chart" :style="{width: boxWidth +'px'}">
+          <div class="tip">{{item.name}}</div>
+          <BarChart :boxWidth="300"/>
+        </div>
+        <div>
+          <div class="charBox flex" v-for="(row, idx) in item.workshop" :key="idx">
+            <div class="chart" :style="{width: boxWidth +'px'}">
+              <div class="tip">{{row.name}}</div>
+              <BarChart :boxWidth="300"/>
+            </div>
+            <div :style="{width: autoWidth +'px', overflow: 'auto'}" class="flex">
+              <div v-for="(row1, idx1) in row.producLine" :key="idx1">
+                <div class="chart" >
+                    <div class="tip">{{row1.name}}</div>
+                    <BarChart :boxWidth="300"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import Row from './row.vue'
+import BarChart from './charts.vue'
+import {ref, onMounted,nextTick } from 'vue'
+const list = [
+  {id: 1, name: '1000', workshop: [{id: 1-1, name: 'M15', producLine: [{id: 1-1-1, name: 'Bo1'}]}]},
+  {id: 2, name: 'FST', workshop: [{id: 2-1, name: 'SJ1', producLine: [{id: 2-1-1, name: 'Ao1'}, {id: 2-1-2, name: 'Ao2'},{id: 2-1-3, name: 'Ao5'}]},{id: 2-2, name: 'SJ2', producLine: [{id: 2-2-1, name: 'Ao3'},{id: 2-2-2, name: 'Ao4'}]}]},
+]
+let boxWidth = ref(0)
+let boxRef = ref(null)
+let autoRef = ref(null)
+let autoWidth = ref(0)
+onMounted(() => {
+  const resizeHandler = () => {
+    boxWidth.value = boxRef.value.offsetWidth
+    autoWidth.value = autoRef.value.offsetWidth
+  }
+  window.addEventListener('resize', resizeHandler)
+  nextTick(() => {
+    boxWidth.value = boxRef.value.offsetWidth
+    autoWidth.value = autoRef.value.offsetWidth
+  })
+})
 </script>
 
 <style lang="scss" scoped>
@@ -13,5 +63,64 @@
     display: flex;
     flex-wrap: wrap;
     background: #343541;
+  }
+  .auto-width {
+    width: calc(100% - 840px)!important;
+    max-width: unset!important;
+    min-width: 35%;
+    overflow: auto;
+  }
+  .defalt-width {
+    width: 400px;
+    max-width: 28%;
+  }
+  .card{
+    border-radius: 4px;
+    margin: 5px;
+    box-shadow: 3px 3px 10px 0 rgba(58, 59, 69, 0.15);
+    width: 100%;
+    &-title {
+      text-align: center;
+      background: #10904a;
+      padding: 5px;
+      color: #fff;
+      margin-left: 10px;
+    }
+    /* 整个滚动条 */
+    ::-webkit-scrollbar {
+      height: 5px; /* 设置滚动条的宽度 */
+    }
+    
+    /* 滚动条轨道 */
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1; /* 设置滚动条轨道的背景颜色 */
+    }
+    
+    /* 滚动条的滑块 */
+    ::-webkit-scrollbar-thumb {
+      background: #888; /* 设置滚动条滑块的背景颜色 */
+    }
+    
+    /* 当鼠标悬停在滑块上 */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #b1afaf; /* 设置滚动条滑块的背景颜色 */
+    }
+  }
+  .charBox {
+    .chart {
+      padding-top: 5px;
+      margin-top: 10px;
+      background-color: #ebffea;
+      margin-left: 10px;
+    }
+  }
+  .tip {
+    padding: 4px;
+    margin-left: 5px;
+    background: #2fc06f;
+    width: 70px;
+    text-align: center;
+    font-size: 14px;
+    color: #fff;
   }
 </style>
